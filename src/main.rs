67e1e4ecc875447dcc15222469356970962caa7a
Oniya Daniel <daniel.prince84@gmail.com>
2022-11-22 19:45:35 +0100

    basic variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    let mut x = 45;
+    let mut x:u64 = 45;
+    let f = 6.7;
+    let b:bool = false;
     println!("The value of x is {}", x);
+    println!("We have a float value f of :: {}", f);
+    println!("We also have a bool whose value is :: {}", b);
 
     x = 60;
     println!("The NEW value of x is {}", x)
